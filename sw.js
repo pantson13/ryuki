52a2958086a8e3f034812d84bb9a87eefab61ff0
pantson13 <pga9156@gmail.com>
2026-08-20 10:09:44 +0800

    Update build version from 126 to 127

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Ryuki v126: atomic core update + full current-audio resync.
-const BUILD = "126";
+const BUILD = "127";
 const CACHE_PREFIX = "ryuki-pwa-";
-const CACHE_NAME = "ryuki-pwa-v126-stable";
+const CACHE_NAME = "ryuki-pwa-v127-stable";
 const INSTALL_CACHE_NAME = `${CACHE_NAME}-install`;
 const INDEX_FALLBACK = `./index.html?appv=${BUILD}`;
 
