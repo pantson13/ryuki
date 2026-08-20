2bcd83314cb6f2b91db03ce9c367c4b7221eb794
pantson13 <pga9156@gmail.com>
2026-08-20 13:44:34 +0800

    Update build version to 136 and cache name

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Ryuki v126: atomic core update + full current-audio resync.
-const BUILD = "135";
+const BUILD = "136";
 const CACHE_PREFIX = "ryuki-pwa-";
-const CACHE_NAME = "ryuki-pwa-v135-stable";
+const CACHE_NAME = "ryuki-pwa-v136-stable";
 const INSTALL_CACHE_NAME = `${CACHE_NAME}-install`;
 const INDEX_FALLBACK = `./index.html?appv=${BUILD}`;
 
